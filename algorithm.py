2356dcde18b8501d0f68be5077a6aa534baf9492
nlange2021@gmail.com <nlange2021@gmail.com>
2024-11-27 16:22:04 -0700

    filesystem

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,21 @@
-#Use language to identify like businesses by title
-#Based on language recommend MSB activities
+import requests
+import pandas as pd
+import csv
 
-def main():
-    f = open('https://fincen.gov/msb-state-selector.html')
+#download data (bytes) from fincen.gov
+csv_url = "https://msb.fincen.gov/#"
+req = requests.get(csv_url)
+url_content = req.content
+data = url_content
+
+#data bytes to string
+encoding = 'utf-8'
+data = data.decode(encoding)
+print(type(data))
+
+with open('fincen_data.csv', 'w', newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(data)
+
+print("data scraped successfully")
+print(data)
