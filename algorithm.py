02eb39b218e5c79ea8dfbedd2e871cb73da4010b
nlange2021@gmail.com <nlange2021@gmail.com>
2024-12-12 02:20:35 -0700

    Program now takes business name, gets the correct probability for each activity being appropriate, orders the activities by probability, and displays all this on the front end to the user.

--- algorithm.py
+++ algorithm.py
@@ -5,36 +5,19 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import glob
 import os
-from website.views import bname
+from sklearn.metrics import classification_report
+from sklearn.naive_bayes import MultinomialNB
 
-#1. Get most recent data
-def get_most_recent_download(save_folder):
-    files = glob.glob(save_folder + '/*')
-    max_file = max(files, key=os.path.getctime)
-    filename = max_file
-    return filename
-filename = get_most_recent_download('downloads')
-#print(filename)
-# All data at disposal
-""" entries = pd.read_csv(filename, encoding='latin1')[['LEGAL NAME','DBA NAME','STREET ADDRESS','CITY',
-                                                   'STATE','ZIP','MSB ACTIVITIES','STATES OF MSB ACTIVITIES',
-                                                   'ALL STATES & TERRITORIES & FOREIGN FLAG**','FOREIGN LOCATION',
-                                                   '# OF BRANCHES','AUTH SIGN DATE','RECEIVED DATE']]
-entries.columns = ['LEGAL NAME','DBA NAME','STREET ADDRESS','CITY',
-                'STATE','ZIP','MSB ACTIVITIES','STATES OF MSB ACTIVITIES',
-                'ALL STATES & TERRITORIES & FOREIGN FLAG**','FOREIGN LOCATION',
-                '# OF BRANCHES','AUTH SIGN DATE','RECEIVED DATE'] """
-# Make a list from 'MSB ACTIVITIES' column - to select
-# Choices of data - 'DBA NAME' is entry, 'MSB ACTIVITIES' is class
-entries = pd.read_csv(filename, encoding='latin1')[['DBA NAME', 'MSB ACTIVITIES']]
-entries.columns = ['entry', 'class']
-#entries = entries[(entries['class'] == '405') | (entries['class'] == '408')] results in just two classes
-#entries = entries[entries['class'].str.contains('405', na=False)] results in all the classes
-entries = entries[(entries['class'] == (lambda x: '405' in x)) | (entries['class'] != (lambda x: '405' in x))]
-entries = entries.reset_index(drop=True)
-""" print(entries.tail()) """
+#for step 4
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+stop = stopwords.words('english')
+stop = stop + [u'a',u'b',u'c',u'd',u'e',u'f',u'g',u'h',u'i',u'j',u'k',u'l',u'm',u'n',u'o',u'p',u'q',u'r',u's',u't',u'v',u'w',u'x',u'y',u'z']
+import nltk
+nltk.download('wordnet')
 
-#2. Remove any html and emoticons from 'DBA NAME' entry
+#for step 2
 import re
 def preprocessor(text):
     if not isinstance(text, str):   #checks for NaN data
@@ -44,84 +27,130 @@ def preprocessor(text):
     text = re.sub('[\W]+', ' ', text.lower()) +\
         ' '.join(emoticons).replace('-', '')
     return text
-""" print(preprocessor("</a>This :) is :( a test :-)!"))
-print(entries['entry']) """
-entries['entry'] = entries['entry'].apply(preprocessor)
+    """ print(preprocessor("</a>This :) is :( a test :-)!"))
+    print(entries['entry']) """      
 
-# Also remove rows with NaN values in 'MSB Activities' class
-entries = entries.dropna(subset=['class'])
-# Make it Binary data
-for i in range(0,len(entries['class'])):
-    if '405' in entries['class'][i]:
-        entries.at[i, 'class'] = 'True'
-    else:
-        entries.at[i, 'class'] = 'False'
+class Model(object):
+    output = []
+    def __init__(self, input):
+        #2*
+         
+        #1. Get most recent data
+        def get_most_recent_download(save_folder):
+            files = glob.glob(save_folder + '/*')
+            max_file = max(files, key=os.path.getctime)
+            filename = max_file
+            return filename
+        filename = get_most_recent_download('downloads')
+        #print(filename)
+        # All data at disposal
+        """ entries = pd.read_csv(filename, encoding='latin1')[['LEGAL NAME','DBA NAME','STREET ADDRESS','CITY',
+                                                        'STATE','ZIP','MSB ACTIVITIES','STATES OF MSB ACTIVITIES',
+                                                        'ALL STATES & TERRITORIES & FOREIGN FLAG**','FOREIGN LOCATION',
+                                                        '# OF BRANCHES','AUTH SIGN DATE','RECEIVED DATE']]
+        entries.columns = ['LEGAL NAME','DBA NAME','STREET ADDRESS','CITY',
+                        'STATE','ZIP','MSB ACTIVITIES','STATES OF MSB ACTIVITIES',
+                        'ALL STATES & TERRITORIES & FOREIGN FLAG**','FOREIGN LOCATION',
+                        '# OF BRANCHES','AUTH SIGN DATE','RECEIVED DATE'] """
+        # Make a list from 'MSB ACTIVITIES' column - to select
+        # Choices of data - 'DBA NAME' is entry, 'MSB ACTIVITIES' is class
+        entries = pd.read_csv(filename, encoding='latin1')[['DBA NAME', 'MSB ACTIVITIES']]
+        entries.columns = ['entry', 'class']
 
-#3. Reindex
-import numpy as np
+        # Also remove rows with NaN values in 'MSB Activities' class
+        entries = entries.dropna(subset=['class'])
 
-entries = entries.reindex(np.random.permutation(entries.index))
+        # Record all options
+        msb_options = set()
+        for eachs in entries['class']:
+            list = str(eachs).split(' ')
+            for each in list:
+                if each != 'nan':
+                    msb_options.add(each)
 
-""" print(entries.head())
-print(entries.tail()) """
+        for msb_option in msb_options:
+            print(msb_option)
+            # 1. Reload Data
+            entries = pd.read_csv(filename, encoding='latin1')[['DBA NAME', 'MSB ACTIVITIES']]
+            entries.columns = ['entry', 'class']
+            #entries = entries[(entries['class'] == '405') | (entries['class'] == '408')] results in just two classes
+            #entries = entries[entries['class'].str.contains('405', na=False)] results in all the classes
+            entries = entries[(entries['class'] == (lambda x: msb_option in x)) | (entries['class'] != (lambda x: msb_option in x))]
+            entries = entries.reset_index(drop=True)
+            """ print(entries.tail()) """
 
-#4. Stopwords
-import nltk
-nltk.download('stopwords')
+            #2. Remove any html and emoticons from 'DBA NAME' entry
+            entries['entry'] = entries['entry'].apply(preprocessor)
 
-from nltk.corpus import stopwords
+            # Make it Binary data
+            for i in range(0,len(entries['class'])):
+                if str(msb_option) in str(entries['class'][i]):
+                    entries.at[i, 'class'] = 'True'
+                else:
+                    entries.at[i, 'class'] = 'False'
 
-stop = stopwords.words('english')
-stop = stop + [u'a',u'b',u'c',u'd',u'e',u'f',u'g',u'h',u'i',u'j',u'k',u'l',u'm',u'n',u'o',u'p',u'q',u'r',u's',u't',u'v',u'w',u'x',u'y',u'z']
+            #3. Reindex
+            import numpy as np
 
-import nltk
-nltk.download('wordnet')
-def split_into_lemmas(entry):
-    entry = str(entry).lower()
-    words = TextBlob(entry).words
-    # for each word, take its "base form" = lemma
-    return [word.lemma for word in words if word not in stop]
+            entries = entries.reindex(np.random.permutation(entries.index))
 
-entries.entry.head().apply(split_into_lemmas)
+            """ print(entries.head())
+            print(entries.tail()) """
 
-#5.
-bow_transformer = CountVectorizer(analyzer=split_into_lemmas).fit(entries['entry'])
-#print(len(bow_transformer.vocabulary_))
+            #4*. Stopwords
+            def split_into_lemmas(entry):
+                entry = str(entry).lower()
+                words = TextBlob(entry).words
+                # for each word, take its "base form" = lemma
+                return [word.lemma for word in words if word not in stop]
 
-#6.
-entries_bow = bow_transformer.transform(entries['entry'])
-""" print('sparse matrix shape:', entries_bow.shape)
-print('number of non-zeros:', entries_bow.nnz)
-print('sparsity: %.2f%%' % (100.0 * entries_bow.nnz / (entries_bow.shape[0] * entries_bow.shape[1]))) """
+            entries.entry.head().apply(split_into_lemmas)
 
-# Get the size of entries and factor into test size
-rows, columns = entries.shape
-train_allocation = int(.8 * rows)
-entries_bow_train = entries_bow[:train_allocation]
-entries_bow_test = entries_bow[train_allocation:]
-entries_class_train = entries['class'][:train_allocation]
-entries_class_test = entries['class'][train_allocation:]
+            #5.
+            bow_transformer = CountVectorizer(analyzer=split_into_lemmas).fit(entries['entry'])
+            #print(len(bow_transformer.vocabulary_))
 
-""" print(entries_bow_train.shape)
-print(entries_bow_test.shape) """
+            #6.
+            entries_bow = bow_transformer.transform(entries['entry'])
+            """ print('sparse matrix shape:', entries_bow.shape)
+            print('number of non-zeros:', entries_bow.nnz)
+            print('sparsity: %.2f%%' % (100.0 * entries_bow.nnz / (entries_bow.shape[0] * entries_bow.shape[1]))) """
 
-#7. 
-from sklearn.metrics import classification_report
-from sklearn.naive_bayes import MultinomialNB
-entries_class = MultinomialNB().fit(entries_bow_train, entries_class_train)
+            # Get the size of entries and factor into test size
+            rows, columns = entries.shape
+            train_allocation = int(.8 * rows)
+            entries_bow_train = entries_bow[:train_allocation]
+            entries_bow_test = entries_bow[train_allocation:]
+            entries_class_train = entries['class'][:train_allocation]
+            entries_class_test = entries['class'][train_allocation:]
 
-predictions = entries_class.predict(entries_bow_test)
+            """ print(entries_bow_train.shape)
+            print(entries_bow_test.shape) """
 
-#print(classification_report(entries_class_test, predictions))
+            #7. 
+            entries_class = MultinomialNB().fit(entries_bow_train, entries_class_train)
 
-#8. 
-def predict_entry(new_entry):
-    new_sample = bow_transformer.transform([new_entry])
-    print(new_entry, np.around(entries_class.predict_proba(new_sample), decimals=5),"\n")
+            predictions = entries_class.predict(entries_bow_test)
 
-# Submit an entry
-def submit(input):
-    predict_entry(input)
+            # print(classification_report(entries_class_test, predictions))
 
-input = bname
-submit(input)
+            #8. 
+            def predict_entry(new_entry):
+                new_sample = bow_transformer.transform([new_entry])
+                result = str(np.around(entries_class.predict_proba(new_sample), decimals=5))
+                #remove bracket, keep the prob True, float, rounded 2 places after decimal, *100, and str
+                result = str(float("{:.2f}".format(float((result[2:]).split(' ', 1)[0])*100)))
+                return('There is a '+ result + ' percent chance '+ str(msb_option) + ' is right for you!')
+                #print(new_entry, np.around(entries_class.predict_proba(new_sample), decimals=5),"\n")
+
+            input = ''
+            self.output.append(predict_entry(input))
+            
+        #9. Sort the output by probabilities reported
+        def extract_first_number(string):
+            # Use regex to find the first number in the string
+            match = re.search(r'\d+', string)
+            return int(match.group()) if match else 0  # Return the number, or 0 if no number is found
+
+        # Sort the strings based on the first number, from largest to smallest
+        self.output = sorted(self.output, key=extract_first_number, reverse=True)
--- website/views.py
+++ website/views.py
@@ -4,14 +4,12 @@ from .models import Note
 from .models import Business
 from . import db
 import json
-import pandas as pd
-import numpy as np
-from textblob import TextBlob
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-import glob
+
+import sys
 import os
-from website.views import bname
+# Go two dirs up
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from algorithm import Model
 
 views = Blueprint('views', __name__)
 
@@ -32,26 +30,23 @@ def home():
 
     return render_template("home.html", user=current_user)
 
-output = ''
-email = ''
-bname = ''
-email = ''
 @views.route('msb', methods=['GET', 'POST'])
 def msb():
     if request.method == 'POST': 
         fname = request.form.get('fname')
         lname = request.form.get('lname')
-        global email
-        global bname
         bname = request.form.get('bname')
         email = request.form.get('email')
         #newb = Business(data=bname, user_id=current_user.id)
         #db.session.add(newb)
         #db.session.commit()
         flash('Data Received', category='success')
-        get_output()
-
-    return render_template("msb.html", user=current_user)
+        input = str(bname)
+        output = Model(input).output
+        output.sort(reverse=True)
+    else:
+        output = ['Please Enter Info']
+    return render_template("msb.html", user=current_user, output=output)#, output=alg_output)   #takes output
 
 @views.route('meet', methods=['GET', 'POST'])
 def meet():
@@ -69,155 +64,3 @@ def delete_note():
             db.session.commit()
 
     return jsonify({})
-
-def get_output():
-    #1. Get most recent data
-    def get_most_recent_download(save_folder):
-        files = glob.glob(save_folder + '/*')
-        max_file = max(files, key=os.path.getctime)
-        filename = max_file
-        return filename
-    filename = get_most_recent_download('downloads')
-    #print(filename)
-    # All data at disposal
-    """ entries = pd.read_csv(filename, encoding='latin1')[['LEGAL NAME','DBA NAME','STREET ADDRESS','CITY',
-                                                    'STATE','ZIP','MSB ACTIVITIES','STATES OF MSB ACTIVITIES',
-                                                    'ALL STATES & TERRITORIES & FOREIGN FLAG**','FOREIGN LOCATION',
-                                                    '# OF BRANCHES','AUTH SIGN DATE','RECEIVED DATE']]
-    entries.columns = ['LEGAL NAME','DBA NAME','STREET ADDRESS','CITY',
-                    'STATE','ZIP','MSB ACTIVITIES','STATES OF MSB ACTIVITIES',
-                    'ALL STATES & TERRITORIES & FOREIGN FLAG**','FOREIGN LOCATION',
-                    '# OF BRANCHES','AUTH SIGN DATE','RECEIVED DATE'] """
-    # Make a list from 'MSB ACTIVITIES' column - to select
-    # Choices of data - 'DBA NAME' is entry, 'MSB ACTIVITIES' is class
-    entries = pd.read_csv(filename, encoding='latin1')[['DBA NAME', 'MSB ACTIVITIES']]
-    entries.columns = ['entry', 'class']
-    #entries = entries[(entries['class'] == '405') | (entries['class'] == '408')] results in just two classes
-    #entries = entries[entries['class'].str.contains('405', na=False)] results in all the classes
-    entries = entries[(entries['class'] == (lambda x: '405' in x)) | (entries['class'] != (lambda x: '405' in x))]
-    entries = entries.reset_index(drop=True)
-    """ print(entries.tail()) """
-
-    #2. Remove any html and emoticons from 'DBA NAME' entry
-    import re
-    def preprocessor(text):
-        if not isinstance(text, str):   #checks for NaN data
-            return ''
-        text = re.sub('<[^>]*>', '', text)
-        emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
-        text = re.sub('[\W]+', ' ', text.lower()) +\
-            ' '.join(emoticons).replace('-', '')
-        return text
-    """ print(preprocessor("</a>This :) is :( a test :-)!"))
-    print(entries['entry']) """
-    entries['entry'] = entries['entry'].apply(preprocessor)
-
-    # Also remove rows with NaN values in 'MSB Activities' class
-    entries = entries.dropna(subset=['class'])
-    # Make it Binary data
-    for i in range(0,len(entries['class'])):
-        if '405' in entries['class'][i]:
-            entries.at[i, 'class'] = 'True'
-        else:
-            entries.at[i, 'class'] = 'False'
-
-    #3. Reindex
-    import numpy as np
-
-    entries = entries.reindex(np.random.permutation(entries.index))
-
-    """ print(entries.head())
-    print(entries.tail()) """
-
-    #4. Stopwords
-    import nltk
-    nltk.download('stopwords')
-
-    from nltk.corpus import stopwords
-
-    stop = stopwords.words('english')
-    stop = stop + [u'a',u'b',u'c',u'd',u'e',u'f',u'g',u'h',u'i',u'j',u'k',u'l',u'm',u'n',u'o',u'p',u'q',u'r',u's',u't',u'v',u'w',u'x',u'y',u'z']
-
-    import nltk
-    nltk.download('wordnet')
-    def split_into_lemmas(entry):
-        entry = str(entry).lower()
-        words = TextBlob(entry).words
-        # for each word, take its "base form" = lemma
-        return [word.lemma for word in words if word not in stop]
-
-    entries.entry.head().apply(split_into_lemmas)
-
-    #5.
-    bow_transformer = CountVectorizer(analyzer=split_into_lemmas).fit(entries['entry'])
-    #print(len(bow_transformer.vocabulary_))
-
-    #6.
-    entries_bow = bow_transformer.transform(entries['entry'])
-    """ print('sparse matrix shape:', entries_bow.shape)
-    print('number of non-zeros:', entries_bow.nnz)
-    print('sparsity: %.2f%%' % (100.0 * entries_bow.nnz / (entries_bow.shape[0] * entries_bow.shape[1]))) """
-
-    # Get the size of entries and factor into test size
-    rows, columns = entries.shape
-    train_allocation = int(.8 * rows)
-    entries_bow_train = entries_bow[:train_allocation]
-    entries_bow_test = entries_bow[train_allocation:]
-    entries_class_train = entries['class'][:train_allocation]
-    entries_class_test = entries['class'][train_allocation:]
-
-    """ print(entries_bow_train.shape)
-    print(entries_bow_test.shape) """
-
-    #7. 
-    from sklearn.metrics import classification_report
-    from sklearn.naive_bayes import MultinomialNB
-    entries_class = MultinomialNB().fit(entries_bow_train, entries_class_train)
-
-    predictions = entries_class.predict(entries_bow_test)
-
-    #print(classification_report(entries_class_test, predictions))
-
-    #8. 
-    def predict_entry(new_entry):
-        new_sample = bow_transformer.transform([new_entry])
-        #print(new_entry, np.around(entries_class.predict_proba(new_sample), decimals=5),"\n")
-        return entries_class.predict_proba(new_sample)
-
-    # Submit an entry
-    def submit(input):
-        global output
-        output = predict_entry(input)
-        return output
-
-    input = bname
-    global output
-    output = submit(input)
-
-    import smtplib
-    #SERVER = "localhost"
-
-    FROM = 'BusinessRaccoon.com'
-
-    global email
-    TO = [email] # must be a list
-
-    SUBJECT = "Here is the suggested msb activities for your MSB!"
-
-    TEXT = output +""
-
-    # Prepare actual message
-
-    message = """\
-    From: %s
-    To: %s
-    Subject: %s
-
-    %s
-    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
-
-    # Send the mail
-
-    server = smtplib.SMTP('myserver')
-    server.sendmail(FROM, TO, message)
-    server.quit()
